1569cf76a267c1e5f89f554ad9e86f6b660028bf
Johnny Dong <johnnydong@dhcp-128-189-68-78.ubcsecure.wireless.ubc.ca>
2025-11-10 13:04:59 -0800

    queried members table

--- portal/app/page.tsx
+++ portal/app/page.tsx
@@ -7,6 +7,10 @@ import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-ste
 import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
+import { createClient } from "@/lib/supabase/server";
+
+const supabase = await createClient();
+const { data: members } = await supabase.from("members").select("*");
 
 export default function Home() {
   return (
@@ -25,6 +29,9 @@ export default function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
+
+          <div>{`${JSON.stringify(members)}`}</div>
+
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
--- portal/app/protected/page.tsx
+++ portal/app/protected/page.tsx
@@ -21,6 +21,7 @@ export default async function ProtectedPage() {
           user
         </div>
       </div>
+
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
